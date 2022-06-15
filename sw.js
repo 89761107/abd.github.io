3f4864123cf55656fb20cab966a2206ae19c941d
89761107 <60531946+89761107@users.noreply.github.com>
2022-06-15 22:28:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-self.addEventListener('install', e => {
-  self.skipWaiting()
-})
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js'
+)
 
-self.addEventListener('activate', e => {
-  self.registration
-    .unregister()
-    .then(() => {
-      return self.clients.matchAll()
-    })
-    .then(clients => {
-      clients.forEach(client => client.navigate(client.url))
-    })
-})
+const ALLOWED_HOSTS = [
+  // The domain to load markdown files
+  location.host,
+  // The domain to load docute
+  'unpkg.com'
+]
+
+const matchCb = ({ url, event }) => {
+  return event.request.method === 'GET' && ALLOWED_HOSTS.includes(url.host)
+}
+
+workbox.routing.registerRoute(
+  matchCb,
+  workbox.strategies.networkFirst()
+)
